8b509466672671a2f64f6d054e1608d018f61372
mar0709 <126666046+mar0709@users.noreply.github.com>
2023-03-15 10:34:09 +0100

    Modificacion hecha desde GitHub

--- java proyect/src/HolaMundo.java
+++ java proyect/src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola q tal");
+		System.out.println("Hola q tal desde GitHub");
 		int num=7;
 		System.out.println("Valor de la variable "+("rnum+9"));
 	}	
